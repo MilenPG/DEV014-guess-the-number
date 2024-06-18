115713ae9cfdf71141c2358af7f085a05accbee5
Milen <millalen.pg@gmail.com>
2024-06-17 21:45:20 -0400

    new method clueMessage() at GuessTheNumberGame, to refactor the given message after a guess. Also removed extra comments

--- src/GuessTheNumberGame.java
+++ src/GuessTheNumberGame.java
@@ -3,7 +3,6 @@ import java.util.ArrayList; //luego dentro de la clase donde se usará, se debe 
 import java.util.Scanner;
 
 public class GuessTheNumberGame {
-    // buena práctica: generar atributos y sus datos con acceso "private" o "protected"
     private Random random; //atributo 1: util.Random = hacer uso de librería de Java para crear nro. aleatorio - random tiene su propio tipo de dato "Random"
     private int targetNumber;
 
@@ -21,43 +20,24 @@ public class GuessTheNumberGame {
           endOfTheGame = true;
       }
       else {
-//        if (player.lastGuess > this.targetNumber) {
-//            System.out.println("Too high!");
-//        } else if (player.lastGuess < this.targetNumber) {
-//            System.out.println("Too low!");
-//        }
         endOfTheGame = false;
      }
       return endOfTheGame;
     }
-            //Elaborar lógica con if para evaluar cada respuesta
-            //if guess === targetNumber return true
-        /*
-        1. checkGuesses, dentro de un while, lo debo usar 2 veces: 1 para evaluar intento humanGuess y otra para computerGuess
-        2. Dentro del mismo while, debo implementar condicional para ganador y fin del juego.
-         (para pruebas, podría acortar el rango de adivinanza)
-    */
-
-    /*
-        1. HumanPlayer hace su intento
-        2. Evaluar intento HP
-            2.1. Si intento = TN ---> HP gana.
-            2.2. Si intento != TN ---> Comunicar "too low" o "too high" en caso respectivo. Continúa el juego.
-        3. CP hace su intento.
-        4. Evaluar intento CP
-            4.1. Si intento = TN ---> CP gana.
-            4.2. Si intento != TN ---> Comunicar "too low" o "too high" en caso respectivo. Continúa el juego.
-        5. Evaluar si HP || CP gana ---> Terminar ciclo. Fin del juego.
-        6. Si no ocurre lo anterior, volver a paso 1.
-    * El ciclo ha de ejecutarse hasta que exista un ganador. While es el tipo de ciclo que nos permite implementar una condicional a la sucesión del ciclo (a diferencia de for q precisa un límite establecido de repeticiones, por eso no lo usamos)
-    */
+    public void clueMessage(Player player) {
+        if (player.lastGuess > this.targetNumber) {
+            System.out.println("Too high!\n");
+        } else if (player.lastGuess < this.targetNumber) {
+            System.out.println("Too low!\n");
+        }
+    }
 
     public static void main(String [] args) {
-        GuessTheNumberGame guessTheNumberGame = new GuessTheNumberGame(); // Aquí estamos 'instanciando' la clase, es decir creandola para poder usarla a ella y sus atributos dentro de este encapsulamiento
+        GuessTheNumberGame guessTheNumberGame = new GuessTheNumberGame(); // Aquí estamos 'instanciando' la clase, es decir, creandola para poder usarla a ella y sus atributos dentro de este encapsulamiento
         System.out.println("☆----------------------------------------------------------☆");
         System.out.println("|  G U E S S   T H E   N U M B E R   ☆   T h e   G a m e   |");
         System.out.println("☆--------------------------------------------------------- ☆");
-        System.out.println("\nTo get started, please enter your name here: "); // Agregar scanner para que la persona escriba su nombre y poder usarlo.
+        System.out.println("\nTo get started, please enter your name here: ");
         Scanner nameScanner = new Scanner(System.in);
         final String name = nameScanner.nextLine();
         System.out.println(name+", welcome to the Guess the Number game! \n");
@@ -66,7 +46,7 @@ public class GuessTheNumberGame {
 
         System.out.println("\n[Testing winner number: "+guessTheNumberGame.getTargetNumber()+"]\n");
 
-        ArrayList<Integer> humanGuesses = new ArrayList<Integer>(); //declaramos la variable
+        ArrayList<Integer> humanGuesses = new ArrayList<Integer>();
         ArrayList<Integer> computerGuesses = new ArrayList<Integer>();
         HumanPlayer humanPlayer = new HumanPlayer(name, humanGuesses);
         ComputerPlayer computerPlayer = new ComputerPlayer("Computer", computerGuesses);
@@ -77,19 +57,10 @@ public class GuessTheNumberGame {
             counter++;
             System.out.println("\n---- Round " + counter + " ----");
             humanPlayer.makeGuess();
-            //guessTheNumberGame.checkGuess(humanPlayer);
             if (!guessTheNumberGame.checkGuess(humanPlayer)) {
-                if (humanPlayer.lastGuess > guessTheNumberGame.targetNumber) {
-                    System.out.println("Too high!\n");
-                } else if (humanPlayer.lastGuess < guessTheNumberGame.targetNumber) {
-                    System.out.println("Too low!\n");
-                }
+               guessTheNumberGame.clueMessage(humanPlayer);
                 computerPlayer.makeGuess();
-                if (computerPlayer.lastGuess > guessTheNumberGame.targetNumber) {
-                    System.out.println("Too high!");
-                } else if (computerPlayer.lastGuess < guessTheNumberGame.targetNumber) {
-                    System.out.println("Too low!");
-                }
+                guessTheNumberGame.clueMessage(computerPlayer);
                 if(guessTheNumberGame.checkGuess(computerPlayer)==true){
                     System.out.println("\n***COMPUTER WINS * END OF THE GAME***\n Computer's attempts history: "+computerGuesses);
 
